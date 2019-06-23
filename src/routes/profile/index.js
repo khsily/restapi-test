4a84699857411d3afc0d04a3a0d00447e35eaa64
hs k <khsilyqwerty@gmail.com>
2019-06-23 15:00:20 +0900

    Update endpoint

--- src/routes/profile/index.js
+++ src/routes/profile/index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import Joi from 'joi';
+import { sha512 } from 'js-sha512';
 
 import { User } from '../../db/models';
 import * as error from '../error';
@@ -6,10 +8,11 @@ import { requireAuth } from "../middlewares";
 
 const profile = express.Router();
 
-profile.get('/currentUser', requireAuth, fetchCurrentUser);
+profile.get('/info', requireAuth, fetchCurrentUserInfo);
 profile.get('/info/:user_id', requireAuth, fetchUserInfo);
+profile.put('/info', requireAuth, updateUserInfo);
 
-async function fetchCurrentUser(req, res, next) {
+async function fetchCurrentUserInfo(req, res, next) {
   try {
     const { user_id } = req.authData.user;
     const user = await User.findOne({
@@ -34,6 +37,36 @@ async function fetchUserInfo(req, res, next) {
     next();
   } catch (e) {
     next(error.database(e));
+  }
+}
+
+async function updateUserInfo(req, res, next) {
+  const updateSchema = Joi.object().keys({
+    email: Joi.string().email(),
+    password: Joi.string(),
+    nickname: Joi.string(),
+  });
+
+  try {
+    const body = await Joi.validate(req.body, updateSchema);
+    const { email, password, nickname } = body;
+    const { user_id } = req.authData.user;
+
+    await User.update({
+      email: email,
+      password: password && sha512(password),
+      nickname: nickname,
+    }, { where: { user_id } });
+
+    const user = await User.findOne({
+      where: { user_id },
+    });
+
+    res.locals.payload = user.info;
+    next();
+  } catch (e) {
+    if (e.isJoi) next(error.parameter(e));
+    else next(error.database(e));
   }
 }
 
--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -6,13 +6,11 @@ import jwt from 'jsonwebtoken';
 import { User } from '../../db/models';
 import * as error from '../error';
 import { jwtSecretKey } from "../../config";
-import { requireAuth } from "../middlewares";
 
 const user = express.Router();
 
 user.post('/signin', signin);
 user.post('/signup', signup);
-user.put('/', requireAuth, updateUser);
 
 async function signin(req, res, next) {
   const signinShcema = Joi.object().keys({
@@ -54,36 +52,6 @@ async function signup(req, res, next) {
       email: email,
       password: sha512(password),
       nickname: nickname,
-    });
-
-    res.locals.payload = user.info;
-    next();
-  } catch (e) {
-    if (e.isJoi) next(error.parameter(e));
-    else next(error.database(e));
-  }
-}
-
-async function updateUser(req, res, next) {
-  const updateSchema = Joi.object().keys({
-    email: Joi.string().email(),
-    password: Joi.string(),
-    nickname: Joi.string(),
-  });
-
-  try {
-    const body = await Joi.validate(req.body, updateSchema);
-    const { email, password, nickname } = body;
-    const { user_id } = req.authData.user;
-
-    await User.update({
-      email: email,
-      password: password && sha512(password),
-      nickname: nickname,
-    }, { where: { user_id } });
-
-    const user = await User.findOne({
-      where: { user_id },
     });
 
     res.locals.payload = user.info;
