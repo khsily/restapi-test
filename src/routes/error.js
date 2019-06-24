b4fa88fd2fc43bbd5bac5d1bebf0bff0cb2e7500
hs k <khsilyqwerty@gmail.com>
2019-06-24 11:43:38 +0900

    Add new error handler

--- src/routes/error.js
+++ src/routes/error.js
@@ -1,7 +1,9 @@
+// TODO: 에러 class 및 상속을 통해 에러 관리하기
+
 /**
  * idErr
  *
- * 인딜라이트 표준 에러 오브젝트를 생성합니다.
+ * 표준 에러 오브젝트를 생성합니다.
  * API의 마지막 라우터에서 처리합니다.
  *
  * @param {number} id_code 응답으로 보낼 http 상태 코드
@@ -21,15 +23,19 @@ export const idErr = (code, msg, anyError) => {
  */
 
 export const parameter = err => {
-  return idErr(400, '요청 인자 오류', err);
+  return idErr(400, '[Bad Request] 요청 인자 오류', err);
 }
 
 export const database = err => {
   let errText = err.toString();
   if (err.errors) errText += ` (${err.errors[0].message})`;
-  return idErr(500, '데이터베이스 오류', errText);
+  return idErr(500, '[Internal Server Error] 데이터베이스 오류', errText);
 }
 
 export const auth = err => {
-  return idErr(401, '토큰 인증 오류', err);
+  return idErr(401, '[Unauthorized] 유저 인증 오류', err);
+}
+
+export const owner = err => {
+  return idErr(403, '[Forbidden] 소유자 인증 오류', err);
 }
