61f78c2d3ec1328e390d87c35b362cb4dde433c0
hs k <khsilyqwerty@gmail.com>
2019-07-01 14:03:24 +0900

    Associate Post, LIke

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -7,6 +7,9 @@ import User from './User';
 import Post from './Post';
 import Like from './Like';
 
+Post.hasMany(Like, { foreignKey: 'post_id' });
+Like.belongsTo(Post, { foreignKey: 'post_id' });
+
 export {
   User,
   Post,
